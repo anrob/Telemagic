e71c8aba3f9e38e99ed4a1e743815d105c48d566
Larry Scott <iamjustfresh@gmail.com>
2011-06-27 19:46:15 -0400

    new rails install - blank

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Telemagic::Application.config.secret_token = '0c3411d08681abc516e32436e1f05d12092bdd3312352ff3fa5aa16d85d5efdea3f083e3160573eaab058bf438145c9bacffac0d10a61cc8284591def416f5ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Telemagic::Application.config.session_store :cookie_store, :key => '_Telemagic_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Telemagic::Application.config.session_store :active_record_store
